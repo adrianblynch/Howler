50828d0c369ae26c186f9715bbafc455e4d3cc3e
Charlotte Holmen <charlotte.holmen@creuna.se>
2014-08-14 00:14:57 +0200

    removed localization

--- Howler/urls.py
+++ Howler/urls.py
@@ -2,10 +2,11 @@ from django.conf.urls import include, patterns, url
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.conf import settings
+from django.conf.urls import patterns
 
 admin.autodiscover()
 
-urlpatterns = i18n_patterns('',
+urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('cms.urls')),
 )
