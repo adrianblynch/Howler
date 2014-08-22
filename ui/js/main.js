1378359be0b0f80e3cc698078668044cad75b437
Charlotte Holmen <charlotte.holmen@creuna.se>
2014-08-22 12:22:58 +0200

    main.js

---
+++ ui/js/main.js
@@ -0,0 +1,36 @@
+define('main', ['jquery', 'slideshow', 'mapify', 'activify', 'ajaxloader', 'lazyload'],
+
+    function ($, Slideshow, Mapify, Activify, Ajaxloader, Lazyload) {
+    'use strict';
+
+	var main = {
+
+		documentSetup : function () {
+			$('html').addClass('js');
+		},
+
+		initApps : function () {
+			Slideshow.start();
+			Activify.start();
+			Lazyload.start();
+
+			
+			if (window.location.search === "") {
+				Ajaxloader.start();
+			}
+			
+			//Mapify.start();
+		},
+
+		start : function () {
+			this.documentSetup();
+			this.initApps();
+		}
+	};
+
+
+	$(document).ready(function () {
+		main.start();
+	});
+    
+});
