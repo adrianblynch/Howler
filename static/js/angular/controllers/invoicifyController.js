6fcff2e0b7e75b43c09a4ee32b06ab1bd30a8d41
Charlotte Holmen <holmen.charlotte@gmail.com>
2014-09-28 23:27:35 +0200

    continued on invoice

--- static/js/angular/controllers/invoicifyController.js
+++ static/js/angular/controllers/invoicifyController.js
@@ -7,10 +7,12 @@ angular.module('invoicify')
     scope.activity = "";
     scope.activePostId = "";
     scope.showAddForm = false;
+    
 
 
     $http.get('/static/js/angular/data/posts.json').success(function (data) {
 		scope.posts = data;
+		scope.setTotals();
 	});
 
 
@@ -20,7 +22,7 @@ angular.module('invoicify')
 		scope.showAddForm = false;
 
 		scope.newPost.description = scope.posts[i].description;
-		scope.newPost.hours = scope.posts[i].hours;
+		scope.newPost.amount = scope.posts[i].amount;
 		scope.newPost.price = scope.posts[i].price;
 		
 		scope.activity = "edit";
@@ -39,6 +41,7 @@ angular.module('invoicify')
 			scope.posts.push(newPost);
 		}
 
+		scope.setTotals();
 		scope.newPost = {};
 
 		if (scope.activity === "edit") {
@@ -87,151 +90,12 @@ angular.module('invoicify')
 		scope.posts[i].active = "true";
 	}
 
-
-
-
-
-/*
-	/*
-    
-    var scope = this;
-
-    scope.projects = [];
-    scope.activities = [];
-    scope.inEditMode = false;
-    scope.activity = "";
-
-    $http.get('./data/projects.json').sucess(function (data) {
-		scope.projects = data;
-	});
-
-	$http.get('./data/activities.json').success(function (data) {
-		scope.activities = data;
-	});
-
-    scope.days = [];
-    scope.newPost = {};
-
-	$http.get('./data/days.json').success(function (data) {
-		scope.days = data;
-	});
-
-
-	
-
-
-	this.addTask = function (dayId) {
-
-		if (scope.activity === "edit") {
-			this.saveTask(scope.newPost, scope.dayId, scope.taskId);
-		}
-		this.setAddFormActive(dayId);
-		scope.activity = "add";
-	}
-
-	this.setAddFormActive = function (dayId) {
-		this.hideAllEditForms();
-
-		for (var i = 0; i < scope.days.length; i++) {
-			if (scope.days[i].id === dayId) {
-				scope.days[i].inEditingMode = "true";
-			} else {
-				scope.days[i].inEditingMode = "false";
-			}
-		}
-	}
-
-	this.isAddFormVisible = function (dayId) {
-		for (var i = 0; i < scope.days.length; i++) {
-			if (scope.days[i].id === dayId && scope.days[i].inEditingMode === "true") {
-				return true;
-			}	
-		}
-	}
-
-	this.hideAllAddForms = function () {
-		for (var i = 0; i < scope.days.length; i++) {
-			scope.days[i].inEditingMode = "false";	
+	scope.setTotals = function () {
+		for (var i = 0; i < scope.posts.length; i++ ) {
+			scope.posts[i].total = (scope.posts[i].amount * scope.posts[i].price);
 		}
 	}
 
 
-	this.editTask = function (taskId, dayId) {
-		this.hideAllAddForms();
-		this.setEditFormActive(taskId, dayId);
-		scope.activity = "edit";
-		scope.taskId = taskId;
-		scope.dayId = dayId;
 
-		for (var i = 0; i < scope.days.length; i++) {
-			if (scope.days[i].id === dayId) {
-				scope.newPost.name = scope.days[i].tasks[taskId].name;
-				scope.newPost.activity = scope.days[i].tasks[taskId].activity;
-				scope.newPost.description = scope.days[i].tasks[taskId].description;
-				scope.newPost.hours = scope.days[i].tasks[taskId].hours;
-			}
-		}
-	}
-		
-	this.setEditFormActive = function (taskId, dayId) {
-		for (var i = 0; i < scope.days.length; i++) {
-			for (var y = 0; y < scope.days[i].tasks.length; y++) {
-				if (y === taskId && scope.days[i].id === dayId) {
-					scope.days[i].tasks[y].inEditingMode = "true";
-				} else {
-					scope.days[i].tasks[y].inEditingMode = "false";
-				}
-			}
-		}
-	}
-
-	this.isEditFormVisible = function (taskId, dayId) {
-		for (var i = 0; i < scope.days.length; i++) {
-			for (var y = 0; y < scope.days[i].tasks.length; y++) {
-				if (y === taskId && scope.days[i].id === dayId && scope.days[i].tasks[y].inEditingMode === "true") {
-					return true;
-				}	
-			}
-		}
-	}
-
-	this.hideAllEditForms = function () {
-		for (var i = 0; i < scope.days.length; i++) {
-			for (var y = 0; y < scope.days[i].tasks.length; y++) {
-				scope.days[i].tasks[y].inEditingMode = "false";
-			}	
-		}
-	}
-
-	this.saveTask = function (newPost, dayId, taskId) {
-		if (scope.activity === "edit") {
-			this.hideAllEditForms();
-			for (var i = 0; i < scope.days.length; i++) {
-				if (scope.days[i].id === dayId) {
-					scope.days[i].tasks[taskId] = newPost;
-				}
-			}
-		} else if (scope.activity === "add"){
-			for (var i = 0; i < scope.days.length; i++) {
-				if (scope.days[i].id === dayId) {
-					scope.days[i].tasks.push(newPost);
-				}
-			}
-		}
-		scope.newPost = {};
-
-		if (scope.activity === "edit") {
-			scope.activity = "";
-		}
-	}
-
-
-	this.deleteTask = function (taskId, dayId) {
-		for (var i = 0; i < scope.days.length; i++) {
-			if (scope.days[i].id === dayId) {
-				scope.days[i].tasks.splice(taskId, 1);
-			}
-		}
-	}
-	*/
 }]);
