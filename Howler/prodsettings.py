82700d95d3042a0b837f28bdb069fb08af482ad5
Charlotte Holmen <holmen.charlotte@gmail.com>
2014-10-05 21:19:54 +0200

    removed settings files

--- Howler/prodsettings.py
+++ Howler/prodsettings.py
@@ -169,8 +169,11 @@ MEDIAFILES_DIRS = (
     os.path.join(PROJECT_PATH, 'static'),
 )
 
+# Amazon s3'''
 
-
+AWS_ACCESS_KEY_ID = os.environ.get('AKIAJDEZ7KM7WN73BODQ')
+AWS_SECRET_ACCESS_KEY = os.environ.get('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')
+AWS_STORAGE_BUCKET_NAME = 'howlerweb'
 
 #if not DEBUG:
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
