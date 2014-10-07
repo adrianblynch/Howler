2ba15a58dab8d2dd5cd76871e356c1d75871a022
Charlotte Holmen <charlotte.holmen@creuna.se>
2014-10-07 09:29:35 +0200

    Merge branch 'master' of https://github.com/charlotteholmen/Howler
    Conflicts:
    Howler/devsettings.py
    Howler/settings.py

--- static/js/angular/controllers/invoicifyController.js
+++ static/js/angular/controllers/invoicifyController.js
@@ -1,100 +1,97 @@
 angular.module('invoicify')
-    .controller('InvoicifyController', ['$http', function($http){
+    .controller('InvoicifyController', ['$scope', '$http', function($http){
 
     var scope = this;
     scope.posts = [];
-    scope.newPost = {};
-    scope.activity = "";
-    scope.activePostId = "";
-    scope.showAddForm = false;
-    
+    scope.settings = [];    
 
-
-    $http.get('/static/js/angular/data/posts.json').success(function (data) {
-		scope.posts = data;
+    $http.get('/static/js/angular/data/mainform.json').success(function (data) {
+		scope.posts = data.posts;
+		scope.postsettings = data.settings;
 		scope.setTotals();
 	});
 
-
-	scope.editPost = function (i) {
-
-		scope.setEditFormActive(i);
-		scope.showAddForm = false;
-
-		scope.newPost.description = scope.posts[i].description;
-		scope.newPost.amount = scope.posts[i].amount;
-		scope.newPost.price = scope.posts[i].price;
-		
-		scope.activity = "edit";
-		scope.activePostId = i;
-	}
-
-	
-	scope.deletePost = function (i) {
-		scope.posts.splice(i, 1);
-	}
-
-	scope.savePost = function (newPost, i) {
-		if (scope.activity === "edit") {
-			scope.posts[i] = newPost;		
-		} else if (scope.activity === "add"){
-			scope.posts.push(newPost);
-		}
-
-		scope.setTotals();
-		scope.newPost = {};
-
-		if (scope.activity === "edit") {
-			scope.activity = "";
-		}
-	}
-
-	scope.addPost = function () {
-		
-		scope.showAddForm = true;
-		scope.hideAllEditForms();
-		scope.activity = "add";
-
-		if (scope.activity === "edit") {
-			scope.saveTask(scope.newPost, scope.activePostId);
-			scope.newPost = {};
-		}
-		
-		scope.setAddFormActive();
-	}
-
-	scope.setAddFormActive = function () {
-		scope.hideAllEditForms();
-	}
-	
-	scope.isAddTaskFormVisible = function () {
-		if (scope.showAddForm === true) {
-			return true;
-		}
-	}
-
-	scope.hideAllEditForms = function () {
-		for (var i = 0; i < scope.posts.length; i++ ) {
-			scope.posts[i].active = "false";
-		}
-	}
-
-	scope.isEditFormVisible = function (i) {
-		if (scope.posts[i].active === "true") {
-			return true;
-		}
-	}
-
-	scope.setEditFormActive = function (i) {
-		scope.hideAllEditForms();
-		scope.posts[i].active = "true";
-	}
+	$http.get('/static/js/angular/data/leftrows.json').success(function (data) {
+		scope.leftrows = data;
+	});
 
 	scope.setTotals = function () {
 		for (var i = 0; i < scope.posts.length; i++ ) {
 			scope.posts[i].total = (scope.posts[i].amount * scope.posts[i].price);
 		}
 	}
+
+
+
+
+	scope.editRow = function (i, posts, form) {
+		scope.interuptEditing(posts);
+		posts[i].active = "true";
+		scope.activePostId = i;
+
+		if (form == "main-form") {
+			scope.newPost.description = scope.posts[i].description;
+			scope.newPost.amount = scope.posts[i].amount;
+			scope.newPost.price = scope.posts[i].price;
+		} else if (form = "lefttop-form") {
+			scope.newPost.description = scope.posts[i].description;
+		}
+	}
+
+	scope.interuptEditing = function (posts) {
+		for (var i = 0; i < posts.length; i++ ) {
+			if (posts[i] === "true") {
+				posts[i] = "false";
+				scope.forceUpdate(posts);
+				scope.newPost = {};
+			}
+		}
+	}
+
+	scope.forceUpdate = function (posts) {
+		scope.updateRow(scope.newPost, scope.activePostId, posts);
+	}
+
+	scope.updateRow = function (newPost, i, posts) {
+		posts[i] = newPost;
+		posts[i].active = "false";
+		scope.newPost = {};
+
+		//If main form
+		scope.setTotals();
+	}
+
+	
+
+	scope.addRow = function (posts) {
+		//What form?
+		scope.showAddRow = true;
+		scope.interuptEditing(posts);
+		
+	}
+
+	scope.saveRow = function (newPost, i, posts) {
+		posts.push(newPost);
+		scope.setTotals();
+		scope.newPost = {};
+	}
+
+	scope.deleteRow = function (i, posts) {
+		posts.splice(i, 1);
+	}
+
+	scope.isRowActive = function (i, posts) {
+		if (posts[i].active === "true") {
+			return true;
+		}
+	}
+
+	scope.isAddRowActive = function () {
+		if (scope.showAddRow === true) {
+			return true;
+		}
+	}
+
 
 
 
